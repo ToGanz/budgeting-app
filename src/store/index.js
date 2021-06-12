8620ce57e027e0cfffcdbd55586c26c973ff2f29
tganz <tganz5890@googlemail.com>
2021-06-12 05:02:59 +0200

    import users module in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import plansModule from './modules/Plans/index.js'
+import usersModule from './modules/Users/index.js'
 
 const store = createStore({
   modules: {
-    plans: plansModule
+    plans: plansModule,
+    users: usersModule
   }
 })
 
