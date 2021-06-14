98ce9cd39476a846cd62b66c9974bcda3a7742a4
tganz <tganz5890@googlemail.com>
2021-06-14 04:27:15 +0200

    update nav

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="nav">
     <router-link to="/">Home</router-link>
     |
-    <router-link :to="{ name: 'Plans' }">Budget Plans</router-link>
-    |
-    <router-link :to="{ name: 'Categories' }">Categories</router-link>
-    |
-    <router-link :to="{ name: 'Register' }">Register</router-link>
-    |
     <router-link v-if="!loggedIn" :to="{ name: 'Login' }">
       Login
     </router-link>
-    <button v-else type="button" class="logoutButton" @click="logout">
-      Logout
-    </button> 
+    <span v-else>
+      |
+      <router-link :to="{ name: 'Plans' }">Budget Plans</router-link>
+      |
+      <router-link :to="{ name: 'Categories' }">Categories</router-link>
+      |
+      <router-link :to="{ name: 'Register' }">Register</router-link>
+
+      <button type="button" class="logoutButton" @click="logout">
+        Logout
+      </button>
+    </span>
   </div>
   <router-view />
 </template>
