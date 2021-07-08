d81ca3ccff99496ad25cad30edecd8bd6558d2fb
tganz <tganz5890@googlemail.com>
2021-07-08 09:23:51 +0200

    reinstalled node modules to fix chokidar anymatch error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'prettier',
-    '@vue/prettier',
   ],
   parserOptions: {
     parser: 'babel-eslint'
