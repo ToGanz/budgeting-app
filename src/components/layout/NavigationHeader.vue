1d75c235a213de5f053eedc4056f87c33cc513c2
tganz <tganz5890@googlemail.com>
2021-07-08 23:38:16 +0200

    add tailwinds header with svg images

--- src/components/layout/NavigationHeader.vue
+++ src/components/layout/NavigationHeader.vue
@@ -1,32 +1,193 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <span v-if="!loggedIn">
-      <router-link :to="{ name: 'Register' }">Register</router-link>
-      |
-      <router-link :to="{ name: 'Login' }">
-        Login
-      </router-link>
-    </span>
-    <span v-else>
-      <router-link :to="{ name: 'Plans' }">Budget Plans</router-link>
-      |
-      <router-link :to="{ name: 'Categories' }">Categories</router-link>
-      |
-      <router-link :to="{ name: 'Profile' }">Profile</router-link>
-      |
-      <button type="button" class="logoutButton" @click="logout">
-        Logout
-      </button>
-    </span>
-  </div>
+  <Popover open="true" class="relative bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div
+        class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"
+      >
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <router-link to="/">
+            <span class="sr-only">Workflow</span>
+            <img
+              class="h-8 w-auto sm:h-10"
+              src="@/assets/images/money.svg"
+              alt=""
+            />
+          </router-link>
+        </div>
+        <div class="-mr-2 -my-2 md:hidden">
+          <PopoverButton
+            class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          >
+            <span class="sr-only">Open menu</span>
+            <MenuIcon class="h-6 w-6" aria-hidden="true" />
+          </PopoverButton>
+        </div>
+
+        <PopoverGroup
+          as="nav"
+          v-if="loggedIn"
+          class="hidden md:flex space-x-10"
+        >
+          <router-link
+            :to="{ name: 'Plans' }"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Budget Plans
+          </router-link>
+          <router-link
+            :to="{ name: 'Categories' }"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Categories
+          </router-link>
+          <router-link
+            :to="{ name: 'Profile' }"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Profile
+          </router-link>
+        </PopoverGroup>
+        <div
+          v-if="loggedIn"
+          class="hidden md:flex items-center justify-end md:flex-1 lg:w-0"
+        >
+          <button
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            @click="logout"
+          >
+            Log out
+          </button>
+        </div>
+        <div
+          v-else
+          class="hidden md:flex items-center justify-end md:flex-1 lg:w-0"
+        >
+          <router-link
+            :to="{ name: 'Login' }"
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Sign in
+          </router-link>
+
+          <router-link
+            :to="{ name: 'Register' }"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Sign up
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <transition
+      enter-active-class="duration-200 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-100 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <PopoverPanel
+        focus
+        class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+      >
+        <div
+          class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50"
+        >
+          <div class="pt-5 pb-6 px-5">
+            <div class="flex items-center justify-between">
+              <div>
+                <img
+                  class="h-8 w-auto"
+                  src="@/assets/images/money.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <div class="-mr-2">
+                <PopoverButton
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
+                  <span class="sr-only">Close menu</span>
+                  <XIcon class="h-6 w-6" aria-hidden="true" />
+                </PopoverButton>
+              </div>
+            </div>
+          </div>
+          <div class="py-6 px-5 space-y-6">
+            <div v-if="loggedIn" class="grid grid-cols-2 gap-y-4 gap-x-8">
+              <router-link
+                :to="{ name: 'Plans' }"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+              >
+                Budget Plans
+              </router-link>
+              <router-link
+                :to="{ name: 'Categories' }"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+              >
+                Categories
+              </router-link>
+              <router-link
+                :to="{ name: 'Profile' }"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+              >
+                Profile
+              </router-link>
+            </div>
+            <div v-if="loggedIn">
+              <button
+                class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
+                @click="logout"
+              >
+                Log out
+              </button>
+            </div>
+            <div v-else>
+              <router-link
+                :to="{ name: 'Register' }"
+                class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Sign up
+              </router-link>
+
+              <p
+                class="mt-6 text-center text-base font-medium text-gray-500"
+              >
+                Existing customer?
+                {{ ' ' }}
+                <router-link
+                  :to="{ name: 'Login' }"
+                  class="text-indigo-600 hover:text-indigo-500"
+                >
+                  > Sign in
+                </router-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </PopoverPanel>
+    </transition>
+  </Popover>
 </template>
 
 <script>
 import { authComputed } from '@/store/helpers.js'
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel
+} from '@headlessui/vue'
+import { MenuIcon, XIcon } from '@heroicons/vue/outline'
 
 export default {
+  components: {
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+    MenuIcon,
+    XIcon
+  },
   computed: {
     loggedIn: authComputed['users/loggedIn']
   },
